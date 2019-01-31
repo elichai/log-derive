8782c2aa904e48f1dc57ea257bea5a5ef4aec36f
Elichai Turkel <elichai.turkel@gmail.com>
2019-01-31 12:38:40 +0200

    First version

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 #![recursion_limit="128"]
-
 extern crate proc_macro;
 extern crate syn;
-use proc_macro2::{Span, TokenStream, Punct};
-use syn::{punctuated::Punctuated, token::Comma, buffer::TokenBuffer, *, token};
+use proc_macro2::{Span, TokenStream};
+use syn::{punctuated::Punctuated, token::Comma, *, token};
 use quote::quote;
 use syn::export::quote::ToTokens;
-use log::Level;
 
-// TODO: Change inner function to always be not visible
-// TODO: Add loggings
-// TODO: Let the user control the loggings.
+// TODO: Let the user Customize loggings.
 // TODO: Add log_enabled condition to skip this all thing if not enabled.
+// TODO: Optimize imports. and optimize syn features.
 fn fnargs_into_barename(list: &Punctuated<FnArg, Comma>) -> (TokenStream, bool) {
     let mut cool = Vec::with_capacity(list.len());
     let mut self_based = false;
@@ -29,7 +26,7 @@ fn fnargs_into_barename(list: &Punctuated<FnArg, Comma>) -> (TokenStream, bool) 
                 self_based = true;
 
             }
-            _ => panic!("HAAAAAAAAA"),
+            _ => panic!("Can't Recognize Argument"),
         }
     }
 
@@ -38,9 +35,10 @@ fn fnargs_into_barename(list: &Punctuated<FnArg, Comma>) -> (TokenStream, bool) 
 }
 
 
-fn set_logger(att: String) -> TokenStream {
-    let mut att_str = att.chars();
-    let att_str = att_str.next().unwrap().to_uppercase().to_string() + att_str.as_str();
+fn set_logger(att: &TokenStream) -> TokenStream {
+    let attr = att.to_string().to_lowercase();
+    let mut attr_char = attr.chars();
+    let att_str = attr_char.next().unwrap().to_uppercase().to_string() + attr_char.as_str();
     let mut res = Punctuated::new();
     res.push_value(Ident::new("log", Span::call_site()));
     res.push_punct(token::Colon2{ spans: [Span::call_site(); 2]});
@@ -50,9 +48,9 @@ fn set_logger(att: String) -> TokenStream {
     res.into_token_stream()
 }
 
-fn get_ouside(inner_call: TokenStream, original: &ItemFn, logger: TokenStream) -> ItemFn {
+fn get_ouside(inner_call: &TokenStream, original: &ItemFn, logger: &TokenStream) -> ItemFn {
     let outside_code = quote! {
-        fn cool() {
+        fn temp() {
                 #inner_call
                 log::log!(#logger, "LOG DERIVE: {:?}", res);
                 res
@@ -73,27 +71,26 @@ fn edit_original(mut original: ItemFn) -> ItemFn {
 }
 
 #[proc_macro_attribute]
-pub fn hello(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let attr2 = proc_macro2::TokenStream::from(attr.clone());
-    let mut inside: ItemFn = parse_macro_input!(item as ItemFn);
+pub fn log(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let attr = proc_macro2::TokenStream::from(attr);
+    let inside: ItemFn = parse_macro_input!(item as ItemFn);
     let (inputs, has_self) = fnargs_into_barename(&inside.decl.inputs);
 
-    let logger = set_logger(attr2.to_string());
+    let logger = set_logger(&attr);
     let edited_inside = edit_original(inside.clone());
-    let mut inner_call;
-    if has_self {
-        inner_call = quote!(let res = Self::#edited_inside.ident(#inputs););
-    } else {
-        inner_call = quote!(let res = #edited_inside.ident(#inputs););
-    }
-    let outside = get_ouside(inner_call, &inside, logger);
 
-    println!("{:#?}", inside);
+    let edited_inside_name = &edited_inside.ident;
+    let inner_call = if has_self {
+        quote!(let res = Self::#edited_inside_name(#inputs);)
+    } else {
+        quote!(let res = #edited_inside_name(#inputs);)
+    };
+    let outside = get_ouside(&inner_call, &inside, &logger);
+
     let res = quote! {
         #edited_inside
         #outside
     };
-//    println!("{}", res);
     res.into()
 
 }
