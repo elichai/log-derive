77f3bb5976e14be6e84d4983405b3799cf027a60
Elichai Turkel <elichai.turkel@gmail.com>
2020-07-15 11:42:28 +0300

    Change async tests to use log assertions

--- tests/test_async.rs
+++ tests/test_async.rs
@@ -1,5 +1,9 @@
 #![cfg(feature = "async_test")]
 
+mod test_logger;
+
+use crate::test_logger::THREAD_LOGGER;
+use log::Level;
 use log_derive::logfn;
 
 #[logfn(INFO)]
@@ -13,15 +17,25 @@ async fn async_function(ok: bool) -> Result<&'static str, &'static str> {
 
 #[test]
 fn async_works() {
-    init_logger();
+    test_logger::init();
 
     futures_executor::block_on(async {
         assert_eq!(async_function(true).await, Ok("async Ok"));
+        THREAD_LOGGER.assert_last_log("async_function() => \"async Ok\"", Level::Info, 9);
         assert_eq!(async_function(false).await, Err("async Err"));
+        THREAD_LOGGER.assert_last_log("async_function() => \"async Err\"", Level::Info, 9);
+        assert!(THREAD_LOGGER.is_empty())
     })
 }
 
-fn init_logger() {
-    // Run with `cargo test -- --test-threads=1 --nocapture`
-    let _ = simplelog::SimpleLogger::init(simplelog::LevelFilter::Info, simplelog::Config::default());
+#[test]
+fn async_works2() {
+    test_logger::init();
+    let block = futures_executor::block_on;
+
+    assert_eq!(block(async_function(true)), Ok("async Ok"));
+    THREAD_LOGGER.assert_last_log("async_function() => \"async Ok\"", Level::Info, 9);
+    assert_eq!(block(async_function(false)), Err("async Err"));
+    THREAD_LOGGER.assert_last_log("async_function() => \"async Err\"", Level::Info, 9);
+    assert!(THREAD_LOGGER.is_empty())
 }
